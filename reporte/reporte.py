a39d23bb89afb2a96032e545d6daeab111177d36
FranciscoCastro07 <castf930@gmail.com>
2026-07-13 21:10:16 -0600

    Update reporte.py
    debe recibir datos nombre, correo, empresa imprimir x, se debe validar los modulos 1 y 2 para realizar import

--- reporte/reporte.py
+++ reporte/reporte.py
@@ -1,1 +1,25 @@
-import : "datos"
+#Grupo 3: Reporte Final#
+def generar_reporte(participantes):
+    """
+    1-Recibir la lista global de participantes procesada. 
+    2-Generar e imprimir un reporte formateado exactamente con la siguiente estructura visual 
+    empleando un ciclo for:
+    """
+    print("============================")
+    print("REGISTRO DEL EVENTO")
+    print("============================")
+    print("Participantes registrados\n")
+
+    # Ciclo for para recorrer y numerar a cada participante
+    for i, persona in enumerate(participantes, 1):
+        print(f"{i}.")
+        print(f"Nombre: {persona['nombre']}")
+        print(f"Empresa: {persona['empresa']}")
+        print(f"Correo: {persona['correo']}")
+        print("---------------------")
+
+    # Conteo final de la lista
+    total = len(participantes)
+    print("============================")
+    print(f"Total registrados: {total}")
+    print("============================")
